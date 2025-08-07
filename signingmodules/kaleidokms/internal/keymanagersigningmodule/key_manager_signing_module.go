12ac25964d1ebc767e4d0a4be569eb82b21c4aae
Anna McAllister <anna.mcallister@kaleido.io>
2025-08-07 09:05:37 +0100

    change paths in kaleido signing module
    Signed-off-by: Anna McAllister <anna.mcallister@kaleido.io>

--- signingmodules/kaleidokms/internal/keymanagersigningmodule/key_manager_signing_module.go
+++ signingmodules/kaleidokms/internal/keymanagersigningmodule/key_manager_signing_module.go
@@ -24,15 +24,15 @@ import (
 
 	"github.com/go-resty/resty/v2"
 
+	"github.com/LF-Decentralized-Trust-labs/paladin/common/go/pkg/i18n"
+	"github.com/LF-Decentralized-Trust-labs/paladin/common/go/pkg/log"
+	"github.com/LF-Decentralized-Trust-labs/paladin/sdk/go/pkg/pldresty"
+	"github.com/LF-Decentralized-Trust-labs/paladin/toolkit/pkg/algorithms"
+	"github.com/LF-Decentralized-Trust-labs/paladin/toolkit/pkg/plugintk"
+	"github.com/LF-Decentralized-Trust-labs/paladin/toolkit/pkg/prototk"
+	"github.com/LF-Decentralized-Trust-labs/paladin/toolkit/pkg/signpayloads"
+	"github.com/LF-Decentralized-Trust-labs/paladin/toolkit/pkg/verifiers"
 	"github.com/kaleido-io/key-manager/signingmodules/kaleidokms/internal/msgs"
-	"github.com/kaleido-io/paladin/common/go/pkg/i18n"
-	"github.com/kaleido-io/paladin/common/go/pkg/log"
-	"github.com/kaleido-io/paladin/sdk/go/pkg/pldresty"
-	"github.com/kaleido-io/paladin/toolkit/pkg/algorithms"
-	"github.com/kaleido-io/paladin/toolkit/pkg/plugintk"
-	"github.com/kaleido-io/paladin/toolkit/pkg/prototk"
-	"github.com/kaleido-io/paladin/toolkit/pkg/signpayloads"
-	"github.com/kaleido-io/paladin/toolkit/pkg/verifiers"
 )
 
 type Server interface {
